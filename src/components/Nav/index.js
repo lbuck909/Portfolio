d389c50aeb5d960d47776e2ddd2e2c82d1c39879
LaiSha Buck <laishabuck@gmail.com>
2023-08-01 12:13:57 -0400

    nav index created

---
+++ src/components/Nav/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Nav({ pages = [], setCurrentPage, currentPage }) {
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentPage.name);
+  }, [currentPage]);
+
+  return (
+    <nav>
+      <ul className="flex-row">
+        {pages.map((page) => (
+          <li
+            className={`mx-5 ${currentPage.name === page.name && 'navActive'}`}
+            key={page.name}
+          >
+            <span onClick={() => setCurrentPage(page)}>
+              {capitalizeFirstLetter(page.name)}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;

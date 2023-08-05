280b43a274d85fc3ac8913553c1bbca82f65b523
LaiSha Buck <laishabuck@gmail.com>
2023-08-05 12:16:48 -0400

    added github and linkedin links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,11 +5,11 @@ function Footer() {
     const icons = [
     {
       name: "fab fa-github",
-      link: "https://github.com/"
+      link: "https://github.com/lbuck909"
     },
     {
       name: "fab fa-linkedin",
-      link: "https://www.linkedin.com/"
+      link: "https://www.linkedin.com/in/laisha-buck-mph7/"
     },
     
   ]

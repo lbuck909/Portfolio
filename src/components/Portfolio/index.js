a7dc8e4afdd2d26cb5df43b9c3c054f4056b36bf
LaiSha Buck <laishabuck@gmail.com>
2023-08-06 13:01:52 -0400

    project index syntax created

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import Project from "../Project";
+
+function Portfolio() {
+
+  const [projects] = useState([
+  {
+    name: '',
+    description:'',
+    link:"",
+    repo: ""
+  },
+  {
+    name: '',
+    description:'',
+    link:"",
+    repo: ""
+  },
+  {
+    name: '',
+    description:'',
+    link:"",
+    repo: ""
+  },
+  {
+    name: '',
+    description:'',
+    link:"",
+    repo: ""
+  },
+]);
+
+return (
+  <div>
+  <div className='flex-row'>
+    {projects.map((project, idx) =>(
+      <Project
+      project={project}
+      key={"project" + idx}
+      />
+    ))}
+  </div>
+  </div>
+)
+};
